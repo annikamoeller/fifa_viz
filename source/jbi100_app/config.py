9efb10e1a871205bc7121ec526c82b44f41c2344
andrei-lucian <lucianandrei12@gmail.com>
2023-12-23 14:26:26 +0100

    global dataframes update

--- source/jbi100_app/config.py
+++ source/jbi100_app/config.py
@@ -1,9 +1,10 @@
 # Here you can add any global configuations
 import pandas as pd
-#color_list1 = ["green", "blue"]
-#color_list2 = ["red", "purple"]
+
+"""Never use these global dataframes directly
+If you want to modify them make a new dataframe 
+(e.g. df = player_stats) and use that instead"""
 
 team_data = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Team Data/team_data.csv', delimiter=',')
 teams_list = team_data['team']
-
 player_stats = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_shooting.csv', delimiter=',')
