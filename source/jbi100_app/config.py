d440d4e84015533961808798a3dac80fc2c6be13
Cristobal Sendin <cristobalsendin@gmail.com>
2024-02-01 17:45:52 +0100

    bugged refactor config

--- source/jbi100_app/config.py
+++ source/jbi100_app/config.py
@@ -11,55 +11,135 @@ team_data = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Te
 player_stats = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_shooting.csv', delimiter=',')
 
 # Player data
+df_player_misc          = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_misc.csv', delimiter=',')
 df_player_defense       = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_defense.csv', delimiter=',')
 df_player_gca           = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_gca.csv', delimiter=',')
-df_player_keepers       = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_keepers.csv', delimiter=',')
-df_player_keepersadv    = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_keepersadv.csv', delimiter=',')
-df_player_misc          = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_misc.csv', delimiter=',')
 df_player_passing       = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_passing.csv', delimiter=',')
-df_player_passing_types = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_passing_types.csv', delimiter=',')
+#df_player_passing_types = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_passing_types.csv', delimiter=',')
 df_player_playingtime   = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_playingtime.csv', delimiter=',')
 df_player_possession    = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_possession.csv', delimiter=',')
 df_player_shooting      = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_shooting.csv', delimiter=',')
 df_player_stats         = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_stats.csv', delimiter=',')
+df_player_keepers       = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_keepers.csv', delimiter=',')
+df_player_keepersadv    = pd.read_csv('../../fifa_viz/FIFA Dataset/Data/FIFA World Cup 2022 Player Data/player_keepersadv.csv', delimiter=',')
 
 # set index to player 
-df_player_stats = df_player_stats.set_index('player')
-df_player_defense =df_player_defense.set_index('player')
 df_player_misc =df_player_misc.set_index('player')
+df_player_defense =df_player_defense.set_index('player')
+df_player_gca = df_player_gca.set_index('player')
 df_player_passing= df_player_passing.set_index('player')
-df_player_shooting= df_player_shooting.set_index('player')
-df_player_possession= df_player_possession.set_index('player')
+#df_player_passing_types = df_player_passing_types.set_index('player')
 df_player_playingtime =df_player_playingtime.set_index('player')
+df_player_possession= df_player_possession.set_index('player')
+df_player_shooting= df_player_shooting.set_index('player')
+df_player_stats = df_player_stats.set_index('player')
+df_player_keepers = df_player_keepers.set_index('player')
+df_player_keepersadv = df_player_keepersadv.set_index('player')
 
+##### MAIN DATA #####
 # remove players with 0 starts
 df_player_playingtime = df_player_playingtime[df_player_playingtime['games_starts']!=0]
 
 # gather useful statistics 
-goals = df_player_stats['goals']
+team = df_player_misc['team']
+position = df_player_misc['position']
+fouls = df_player_misc['fouls']
+own_goals = df_player_misc['own_goals']
+aerials_won = df_player_misc['aerials_won']
+offsides = df_player_misc['offsides']
+
+tackles = df_player_defense['tackles']
+tackles_won = df_player_defense['tackles_won']
+clearances = df_player_defense['clearances']
+blocks = df_player_defense['blocks']
+errors = df_player_defense['errors']
+
+goal_creating_actions = df_player_gca['gca']
+shot_creating_actions = df_player_gca['sca']
+goal_creating_actions_90s = df_player_gca['gca_per90']
+shot_creating_actions_90s = df_player_gca['sca_per90']
+
+
+passes = df_player_passing['passes_completed']
+passes_pct = df_player_passing['passes_pct']
+passes_total_distance = df_player_passing['passes_total_distance']
+passes_pct_long = df_player_passing['passes_pct_long']
+progressive_passes = df_player_passing['progressive_passes']
+
+appearances = df_player_playingtime['games_starts']
+
+touches = df_player_possession['touches']
+dispossessed = df_player_possession['dispossessed']
+aerials_won_pct = df_player_misc['aerials_won_pct']
+dribbles_completed_pct = df_player_possession['dribbles_completed_pct']
+passes_received = df_player_possession['passes_received']
+miscontrols  = df_player_possession['miscontrols']
+
+goals = df_player_shooting['goals']
+shots = df_player_shooting['shots']
+shots_on_target = df_player_shooting['shots_on_target']
+shots_on_target_pct= df_player_shooting['shots_on_target_pct']
+goals_per_shot= df_player_shooting['goals_per_shot']
+average_shot_distance= df_player_shooting['average_shot_distance']
+pens_made= df_player_shooting['pens_made']
+pens_att= df_player_shooting['pens_att']
+
 xg = df_player_stats['xg']
 birth_year = df_player_stats['birth_year']
-goal_creating_actions = df_player_gca['gca']
 assists = df_player_stats['assists']
-appearances = df_player_playingtime['games_starts']
 yellow_cards = df_player_stats['cards_yellow']
 red_cards = df_player_stats['cards_red']
-passes = df_player_passing['passes_completed']
-touches = df_player_possession['touches']
-shots = df_player_shooting['shots']
-offsides = df_player_misc['offsides']
-tackles = df_player_defense['tackles']
-fouls = df_player_misc['fouls']
-dispossessed = df_player_possession['dispossessed']
-own_goals = df_player_misc['own_goals']
-clearances = df_player_defense['clearances']
-aerials_won = df_player_misc['aerials_won']
-position = df_player_misc['position']
-team = df_player_misc['team']
+minutes_90s= df_player_stats['minutes_90s']
 
-# create main dataframe
-main_df = pd.concat([goals, xg, birth_year, assists, appearances, yellow_cards, red_cards, passes, touches, shots, offsides, tackles, fouls, dispossessed, own_goals, clearances, position, team], axis=1)
+
+'''
+#Per 90s statistics
+goals_90s = goals/minutes_90s
+blocks_90s = blocks/minutes_90s
+clearances_90s = clearances/minutes_90s
+severe_error = errors/minutes_90s
+touches_90s = touches/minutes_90s
+miscontrols_90s = miscontrols/minutes_90s
+dispossessed_90s = dispossessed/minutes_90s
+
+#Ratios
+tackle_success = tackles_won/tackles
+'''
+
+'''
+df_attack['Shot Accuracy'] = df_player_shooting['shots_on_target'] / df_player_shooting['shots']
+'''
+
+############
+
+##### INFO CARD DF#####
+df_player_basic = pd.concat([birth_year, team, position, minutes_90s])
+
+
+##### MAIN DF #####
+main_df = pd.concat([goals, xg, birth_year, assists, appearances, yellow_cards, red_cards, passes, touches, shots,
+                     offsides, tackles, fouls, dispossessed, own_goals, clearances, position, team, shot_creating_actions, goal_creating_actions,
+                     tackles, blocks, miscontrols])
+
+#Drop goalkeepers
 main_df = main_df.drop(main_df[main_df['position'] == 'GK'].index)
+
+# fill NaN values with "No data available message"
+main_df.fillna('No data')
+
+
+##### MAIN DF 90S ######
+main_df_90s = pd.DataFrame()
+main_df_90s['player'] = main_df['player']
+
+for column in main_df.columns:
+    if column not in ['player', 'xg', 'birth_year', 'position', 'team']:
+        main_df_90s[column + ' per 90s'] = main_df[column]/minutes_90s
+
+
+##### TABLE DF #####
+table_df = pd.concat(main_df, main_df_90s)
+
 
 # teams for table drop down
 teams_list = list(main_df['team'].unique())
@@ -71,141 +151,10 @@ player_stats.remove('team')
 player_stats.remove('position')
 player_stats = sorted(player_stats)
 
-# fill NaN values with "No data available message"
-main_df.fillna("No data")
 
-# create goalkeeper df
-gk_df = pd.merge(df_player_keepers[['player', 'team', 'position', 'gk_save_pct', 'gk_pens_save_pct']], df_player_keepersadv[['player', 'gk_passes_length_avg', 'gk_goal_kick_length_avg']], on='player', how='inner')
-gk_df = gk_df.set_index('player')
-# statistics for table drop down
-gk_stats = list(gk_df.columns)
-gk_stats.remove('team')
-gk_stats.remove('position')
+##### RADAR DF #####
 
-# basic player info df for info card
-df_player_basic = df_player_misc[['birth_year', 'team', 'position', 'minutes_90s']]
-
-# Globally Normalized Data
-# normalized_main_df = main_df.drop(["team"])#.apply(normalize_df)
-normalized_main_df = main_df.drop(columns=['team', 'position', 'birth_year'])
-normalized_main_df = normalized_main_df.apply(normalize_df)
-
-##### Prepare data for usage in Heatmap (and possibly other widgets)
-# Attacking DF
-df_attack = pd.DataFrame()
-# df_attack['Player'] = df_player_shooting.index
-# df_attack = df_attack.set_index('Player')
-df_attack['Shot Accuracy'] = df_player_shooting['shots_on_target'] / df_player_shooting['shots']
-df_attack['SCA per 90s'] = df_player_gca.set_index('player')['sca_per90']
-df_attack['GCA per 90s'] = df_player_gca.set_index('player')['gca_per90']
-df_attack['Goals per 90s'] = df_player_shooting['goals'] / df_player_shooting['minutes_90s']
-
-# Defensive DF
-df_defense = pd.DataFrame()
-# df_defense['Player'] = df_player_defense.index
-# df_defense = df_defense.set_index('Player')
-df_defense['Tackle Success'] = df_player_defense['tackles_won'] / df_player_defense['tackles']
-df_defense['Blocks per 90s'] = df_player_defense['blocks'] / df_player_defense['minutes_90s']
-df_defense['Clearances per 90s'] = df_player_defense['clearances'] / df_player_defense['minutes_90s']
-df_defense['Severe Error'] = df_player_defense['errors'] / df_player_defense['minutes_90s']
-
-
-# Possesion
-df_possesion = pd.DataFrame()
-# df_possesion['Player'] = df_player_possession.index
-# df_possesion = df_possesion.set_index('Player')
-df_possesion['Touches per 90s'] = df_player_possession['touches'] / df_player_possession['minutes_90s']
-df_possesion['Dribble Success'] = df_player_possession['dribbles_completed_pct']
-df_possesion['Miscontrol per 90s'] = df_player_possession['miscontrols'] / df_player_possession['minutes_90s']
-df_possesion['Dispossessed per 90s'] = df_player_possession['dispossessed'] / df_player_possession['minutes_90s']
-
-
-# Concatenate of dataframes (hm=heatmap)
-df_hm = pd.concat([df_attack, df_defense, df_possesion], axis=1)
-df_hm.replace([np.inf, -np.inf], 0, inplace=True)
-df_hm_norm = df_hm.apply(normalize_df)
-
-
-#Goalkeeper Dataframe for the Heatmap
-df_keepers = df_player_keepers[['player', 'gk_goals_against_per90', 'gk_save_pct', 'gk_clean_sheets_pct', 'gk_pens_save_pct']]
-df_keepers.set_index('player', inplace=True)
-
-df_keepersadv = df_player_keepersadv[['player', 'gk_passes_pct_launched', 'gk_passes_length_avg', 'gk_crosses_stopped_pct']]
-df_keepersadv.set_index('player', inplace=True)
-
-df_gk_hm = pd.concat([df_keepers, df_keepersadv], axis=1)
-df_gk_hm.fillna(0, inplace=True)
-
-df_gk_hm.rename(columns={'gk_goals_against_per90':'Goals conceded per 90s',
-                         'gk_save_pct':'Goals saved percentage',
-                         'gk_clean_sheets_pct':'Clean sheets percentage',
-                         'gk_pens_save_pct': 'Penalty save percentage',
-                         'gk_passes_pct_launched':'Percentage of successful passes',
-                         'gk_passes_length_avg':'Average pass lenght',
-                        'gk_crosses_stopped_pct':'Stopped crosses percentage' }, inplace=True)
-
-df_gk_hm_norm = df_gk_hm.apply(normalize_df)
-
-
-## NAN-Filled Dataframes by median imputation. (main & gk)
-df_hm_filled = df_hm.apply(median_imputation_of_nan)
-df_gk_hm_filled = df_gk_hm.apply(median_imputation_of_nan)
-
-######## Dataframe for radar plot
-minutes = df_player_stats['minutes_90s']
-
-# gather useful statistics 
-#attack
-goals = df_player_shooting['goals']
-shots_on_target_pct= df_player_shooting['shots_on_target_pct']
-goals_per_shot= df_player_shooting['goals_per_shot']
-average_shot_distance= df_player_shooting['average_shot_distance']
-pens_made= df_player_shooting['pens_made']
-pens_att= df_player_shooting['pens_att']
-
-#defense
-tackles = df_player_defense['tackles']
-tackles_won = df_player_defense['tackles_won']
-clearances = df_player_defense['clearances']
-blocks = df_player_defense['blocks']
-errors = df_player_defense['errors']
-
-#control
-touches = df_player_possession['touches']
-dispossessed = df_player_possession['dispossessed']
-aerials_won_pct = df_player_misc['aerials_won_pct']
-dribbles_completed_pct = df_player_possession['dribbles_completed_pct']
-passes_received = df_player_possession['passes_received']
-miscontrols  = df_player_possession['miscontrols']
-
-#passing
-assists = df_player_stats['assists']
-passes_pct = df_player_passing['passes_pct']
-passes_total_distance = df_player_passing['passes_total_distance']
-passes_pct_long = df_player_passing['passes_pct_long']
-progressive_passes = df_player_passing['progressive_passes']
-
-#discipline
-red_cards = df_player_stats['cards_red']
-yellow_cards = df_player_stats['cards_yellow']
-fouls = df_player_misc['fouls']
-
-
-
-# create main dataframe
-df_radar_main = pd.concat([position,
-                     goals, shots_on_target_pct, goals_per_shot, average_shot_distance,pens_made,pens_att,
-                     tackles,tackles_won, clearances, blocks, errors,
-                     touches, dispossessed, aerials_won_pct, dribbles_completed_pct, passes_received, miscontrols,
-                     assists, passes_pct, passes_total_distance, passes_pct_long, progressive_passes,
-                     red_cards, yellow_cards, fouls, minutes
-                     ], axis=1)
-df_radar_main = df_radar_main.drop(df_radar_main[df_radar_main['position'] == 'GK'].index)
-
-
-df_radar_main.replace([np.inf, -np.inf, np.nan], 0, inplace=True)
-
-df_radar = pd.DataFrame()
+radar_df = pd.DataFrame()
 
 def get_attack_score(row):
     if (row['pens_att'] == 0): pens = 0
@@ -267,11 +216,53 @@ def get_discipline_score(row):
     return max(score, 0)
 
 
-df_radar['Attack'] = df_radar_main.apply(get_attack_score, axis=1)
-df_radar['Defense'] = df_radar_main.apply(get_defense_score, axis=1)
-df_radar['Control'] = df_radar_main.apply(get_control_score, axis=1)
-df_radar['Passing'] = df_radar_main.apply(get_passing_score, axis=1)
-df_radar['Discipline'] = df_radar_main.apply(get_discipline_score, axis=1)
+radar_df['Attack'] = table_df.replace([np.inf, -np.inf, np.nan], 0).apply(get_attack_score, axis=1)
+radar_df['Defense'] = table_df.replace([np.inf, -np.inf, np.nan], 0).apply(get_defense_score, axis=1)
+radar_df['Control'] = table_df.replace([np.inf, -np.inf, np.nan], 0).apply(get_control_score, axis=1)
+radar_df['Passing'] = table_df.replace([np.inf, -np.inf, np.nan], 0).apply(get_passing_score, axis=1)
+radar_df['Discipline'] = table_df.replace([np.inf, -np.inf, np.nan], 0).apply(get_discipline_score, axis=1)
+
+
+##########################################################FUCKING GK#############################################################
+
+
+
+# create goalkeeper df
+gk_df = pd.merge(df_player_keepers[['player', 'team', 'position', 'gk_save_pct', 'gk_pens_save_pct']], df_player_keepersadv[['player', 'gk_passes_length_avg', 'gk_goal_kick_length_avg']], on='player', how='inner')
+gk_df = gk_df.set_index('player')
+# statistics for table drop down
+gk_stats = list(gk_df.columns)
+gk_stats.remove('team')
+gk_stats.remove('position')
+
+
+
+
+
+#Goalkeeper Dataframe for the Heatmap
+df_keepers = df_player_keepers[['player', 'gk_goals_against_per90', 'gk_save_pct', 'gk_clean_sheets_pct', 'gk_pens_save_pct']]
+df_keepers.set_index('player', inplace=True)
+
+df_keepersadv = df_player_keepersadv[['player', 'gk_passes_pct_launched', 'gk_passes_length_avg', 'gk_crosses_stopped_pct']]
+df_keepersadv.set_index('player', inplace=True)
+
+df_gk_hm = pd.concat([df_keepers, df_keepersadv], axis=1)
+df_gk_hm.fillna(0, inplace=True)
+
+df_gk_hm.rename(columns={'gk_goals_against_per90':'Goals conceded per 90s',
+                         'gk_save_pct':'Goals saved percentage',
+                         'gk_clean_sheets_pct':'Clean sheets percentage',
+                         'gk_pens_save_pct': 'Penalty save percentage',
+                         'gk_passes_pct_launched':'Percentage of successful passes',
+                         'gk_passes_length_avg':'Average pass lenght',
+                        'gk_crosses_stopped_pct':'Stopped crosses percentage' }, inplace=True)
+
+df_gk_hm_norm = df_gk_hm.apply(normalize_df)
+
+df_gk_hm_filled = df_gk_hm.apply(median_imputation_of_nan)
+
+
+
 
 ##Df for gk radar plot
 df_player_keepers.set_index('player', inplace=True)
@@ -349,6 +340,9 @@ df_gk_radar['Defense'] = main_gk_df.apply(get_defense_score, axis=1)
 df_gk_radar['Kick'] = main_gk_df.apply(get_kick_score, axis=1)
 df_gk_radar['Passing'] = main_gk_df.apply(get_passing_score, axis=1)
 df_gk_radar['Penalty'] = main_gk_df.apply(get_penalty_score, axis=1)
+
+
+#################################################FUCKING GK END #####################################################
 
 def get_similar_players(on, player, num_similar_players=5):
     """
