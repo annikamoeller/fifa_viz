6a22c67ba128fe7e0c093b7edbdf628484102d47
jcsendin <79402454+jcsendin@users.noreply.github.com>
2024-02-02 19:27:26 +0100

    Delete source/jbi100_app/views/player_stats.py

--- source/jbi100_app/views/player_stats.py
+++
@@ -1,9 +0,0 @@
-from dash import dcc, html
-import plotly.graph_objects as go
-import pitchly
-from pitchly.pitch import Pitch
-from PIL import Image
-import plotly.express as px
-import plotly.graph_objects as go
-
-
